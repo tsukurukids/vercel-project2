f7fd55005a618e76a5a649d8b991656238c2b363
tsukurukids <tsukurukids4f@gmail.com>
2024-08-21 18:34:05 +0900

    Update check-progress.js

--- api/check-progress.js
+++ api/check-progress.js
@@ -1,23 +1,33 @@
 export default function handler(req, res) {
   const { name } = req.body;
-  
+
   // ひらがなの正規表現
   const hiraganaRegex = /^[\u3040-\u309F]+$/;
 
-  // 名前が全てひらがなであり、かつスペースがない場合
-  if (hiraganaRegex.test(name) && !/\s/.test(name)) {
-    if (name === "はやさきはると") {
-      // はやさきはるとの場合
-      res.redirect(process.env.tsukuru_h_haruto_URL);
-    } else if (name === "きんじょうきゆ") {
-      // きんじょうきゆの場合
-      res.redirect(process.env.tsukuru_k_kiyu_URL);
+  try {
+    // 名前が全てひらがなであり、かつスペースがない場合
+    if (hiraganaRegex.test(name) && !/\s/.test(name)) {
+      if (name === "はやさきはると") {
+        // 環境変数が正しいURLを持っているかチェック
+        if (process.env.tsukuru_h_haruto_URL) {
+          res.redirect(307, process.env.tsukuru_h_haruto_URL);
+        } else {
+          throw new Error("URL not defined for tsukuru_h_haruto_URL");
+        }
+      } else if (name === "きんじょうきゆ") {
+        if (process.env.tsukuru_k_kiyu_URL) {
+          res.redirect(307, process.env.tsukuru_k_kiyu_URL);
+        } else {
+          throw new Error("URL not defined for tsukuru_k_kiyu_URL");
+        }
+      } else {
+        res.status(400).send("学習進捗が見つかりませんでした");
+      }
     } else {
-      // 名前が一致しなかった場合
-      res.status(400).send("学習進捗が見つかりませんでした");
+      res.status(400).send("名前はひらがなで、スペースなしで入力してください");
     }
-  } else {
-    // 名前がひらがなでない、またはスペースを含んでいる場合のエラーメッセージ
-    res.status(400).send("名前はひらがなで、スペースなしで入力してください");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("サーバーエラーが発生しました");
   }
 }
