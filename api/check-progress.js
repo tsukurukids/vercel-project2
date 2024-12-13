da563105d1a8dc8e8de6a8ef282bf517e8f563a8
tsukurukids <tsukurukids4f@gmail.com>
2024-12-13 17:42:53 +0900

    Update check-progress.js

--- api/check-progress.js
+++ api/check-progress.js
@@ -84,7 +84,9 @@ export default function handler(req, res) {
         } else if (name === "うえだりひと") {
             res.redirect(307, process.env.tsukuru_rihito_URL);
         } else if (name === "くめかいら") {
-            res.redirect(307, process.env.tsukuru_k_kaira_URL);    
+            res.redirect(307, process.env.tsukuru_k_kaira_URL);
+        } else if (name === "なかむらひなた") {
+            res.redirect(307, process.env.tsukuru_n_hinata_URL);    
         } else {
             // 名前が見つからなかった場合のエラーメッセージ
             res.status(404).send(`
